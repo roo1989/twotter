b72abe6fdf524de13336668441d6a168f3ec59fe
roo1989 <ragnar.orn@gmail.com>
2020-10-04 16:18:15 +0200

    Vue3 composition api refactor part 2: UserProfile component

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="user-profile">
     <div class="user-profile__user-panel">
-      <h1 class="user-profile__username">@{{ user.username }}</h1>
-      <div class="user-profile__admin-badge" v-show="user.isAdmin">
-          Admin
+      <h1 class="user-profile__username">@{{ state.user.username }}</h1>
+      <div class="user-profile__admin-badge" v-show="state.user.isAdmin">
+        Admin
       </div>
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
@@ -13,26 +13,27 @@
     </div>
     <!-- TwootItem component -->
     <div class="user-profile__twoots-wrapper">
-        <TwootItem
-            v-for="twoot in user.twoots"
-            :key="twoot.id"
-            :username="user.username"
-            :twoot="twoot"
-            @favourite="favourite"
-        />
+      <TwootItem
+          v-for="twoot in state.user.twoots"
+          :key="twoot.id"
+          :username="state.user.username"
+          :twoot="twoot"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue';
+
 import TwootItem from '@/components/TwootItem.vue';
 import CreateTwootPanel from '@/components/CreateTwootPanel';
 
 export default {
   name: "UserProfile",
   components: {CreateTwootPanel, TwootItem},
-  data() {
-    return {
+  setup() {
+    const state = reactive({
       followers: 0,
       user: {
         id: 1,
@@ -42,33 +43,26 @@ export default {
         email: "ragnar@example.com",
         isAdmin: true,
         twoots: [
-            { id: 1, content: 'Twotter is Amazing!!!' },
-            { id: 2, content: 'Don\'t forget to subscribe to me!' },
-            { id: 3, content: 'What will happen after the coronavirus passes?' },
-        ]
+          {id: 1, content: 'Twotter is Amazing!!!'},
+          {id: 2, content: 'Don\'t forget to subscribe to me!'},
+          {id: 3, content: 'What will happen after the coronavirus passes?'},
+        ],
       },
-    };
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers += 1;
-    },
-    favourite(id) {
-        console.log(`Twoot with id ${id} favoured!`);
-    },
-    addTwoot(twoot) {
-      this.user.twoots.unshift({
-        id: this.user.twoots.length + 1,
-        content: twoot,
+    });
+
+    function addTwoot(twoot) {
+      state.user.twoots.unshift({
+        id: state.user.twoots.length + 1,
+        content: twoot
       });
-    },
-  },
-};
+    }
+
+    return {
+      state,
+      addTwoot
+    };
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -79,31 +73,31 @@ export default {
   grid-gap: 50px;
   padding: 50px 5%;
 
-    .user-profile__user-panel {
-      display: flex;
-      flex-direction: column;
-      padding: 20px;
-      background-color: white;
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+
+    h1 {
+      margin: 0;
+    }
+
+    .user-profile__admin-badge {
+      background-color: rebeccapurple;
+      color: white;
+      padding: 0 10px;
       border-radius: 5px;
-      border: 1px solid #DFE3E8;
-
-        h1 {
-          margin: 0;
-        }
-
-        .user-profile__admin-badge {
-          background-color: rebeccapurple;
-          color: white;
-          padding: 0 10px;
-          border-radius: 5px;
-          margin-right: auto;
-          font-weight: bold;
-        }
+      margin-right: auto;
+      font-weight: bold;
     }
+  }
 
-    .user-profile__twoots-wrapper {
-      display: grid;
-      grid-gap: 10px;
-    }
+  .user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+  }
 }
 </style>
