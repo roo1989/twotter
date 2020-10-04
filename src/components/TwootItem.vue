22c886484807615b6104d2c6b83fe6209b3f2513
roo1989 <ragnar.orn@gmail.com>
2020-10-04 10:32:51 +0200

    Change lang to scss for styling

--- src/components/TwootItem.vue
+++ src/components/TwootItem.vue
@@ -32,23 +32,22 @@
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .twoot-item {
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E4;
-    box-sizing: border-box;
-    cursor: pointer;
-    transition: all 0.25s ease;
-}
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E4;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
 
-.twoot-item:hover {
+  &:hover {
     transform: scale(1.1, 1.1)
-}
+  }
 
-.twoot-item__user {
+  .twoot-item__user {
     font-weight: bold;
+  }
 }
-
 </style>
