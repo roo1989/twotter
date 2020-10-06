6a9da85ebf0eb710cd531d7a8c12600fa7618e3d
roo1989 <ragnar.orn@gmail.com>
2020-10-06 17:57:49 +0200

    Create an admin middleware, to check if user isAdmin and create a route for admin as well as an Admin component

---
+++ src/views/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="admin">Admin</h1>
+</template>
+
+<script>
+export default {
+  name: 'Admin',
+}
+</script>
+
+<style scoped lang='scss'>
+  h1 {
+    padding: 50px 5%;
+  }
+</style>
